316cca8535cf39fdaa1279a259238a352cbd6fb6
Lim Young Rok <xotrs1@naver.com>
2017-08-02 15:17:46 +0900

    React - Redux Counter v2 생성/제거버튼 – Buttons 컴포넌트 만들기

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,26 @@
 import * as types from './ActionTypes';
 
+export const create = (color) => ({
+    type: types.CREATE,
+    color
+})
+
+export const remove = () => ({
+    type: types.REMOVE
+})
+
 export const increment = () => ({
-    type: types.INCREMENT
+    type: types.INCREMENT,
+    index
 })
 
 export const decrement = () => ({
-    type: types.DECREMENT
+    type: types.DECREMENT,
+    index
 })
 
 export const setColor = (color) => ({
     type: types.SET_COLOR,
+    index,
     color
 })
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
